e2192c0ca1c75711f8c058e7f48309addb4cb40f
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2022-10-31 00:18:41 +0530

    Chicks in Zoo

--- Oct_30.cpp
+++ Oct_30.cpp
@@ -1,47 +1,37 @@
-https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
+// https://practice.geeksforgeeks.org/problems/chicks-in-a-zoo1159/1
+
+// Initially, the zoo have a single chick. A chick gives birth to 2 chicks every day and the life expectancy of a chick is 6 days.
+// Zoo officials want to buy food for chicks so they want to know the number of chicks on an Nth day. Help the officials with this task.
+
+// Input: N = 3
+// Output: 9
+// Explanation: First day there is only 1 chick.
+// On second day total number of chicks = 3.
+// On third day total number of chicks = 9
+
+// Input: N = 2 
+// Output: 3
+// Explanation: First day there is only 1 chick.
+// On second day total number of chicks = 3
+
+// Approach -
+// 1. We will create an expires array which will store the number of chicks that will expire on the ith day.
+// 2. Keep a counter of chicks and set it to 1.
+// 3. Run a loop from 1 to n and for each i , decrease the counter by expires[i] and set expires[i + 6] to count * 2.
+// 4. Increment the counter by (counter * 2).
+// 5. Return the counter.
 
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> vis(m , vector<int>(n , -1));
-        queue<vector<int>> q;
-        // represents current row , current col , current length , available k
-        q.push({0 , 0 , 0 , k});
-        while(!q.empty()) {
-            auto it = q.front();
-            q.pop();
-            int x = it[0];
-            int y = it[1];
-            int currLength = it[2];
-            int kAvailable = it[3];
-            // check if it is out of bounds
-            if(x < 0 || y < 0 || x >= m || y >= n) {
-                continue;
-            }
-            // check if reached the destination
-            if(x == m-1 && y == n-1) {
-                return currLength;
-            }
-            // check if obstacle is there
-            if(grid[x][y] == 1) {
-                if(kAvailable > 0) {
-                    kAvailable --;
-                } else {
-                    continue;
-                }
-            }
-            // check if cell is already visited
-            if(vis[x][y] != -1 && vis[x][y] >= kAvailable) {
-                continue;
-            }
-            vis[x][y] = kAvailable;
-            q.push({x , y+1 , currLength + 1 , kAvailable});
-            q.push({x , y-1 , currLength + 1 , kAvailable});
-            q.push({x-1 , y , currLength + 1 , kAvailable});
-            q.push({x+1 , y , currLength + 1 , kAvailable});
-        }
-        return -1;
-    }
+	long long int NoOfChicks(int n){
+	    long long int cnt = 1;
+	    vector<long long int> expires(50 , 0);
+	    expires[6] = 1;
+	    for(int i =1 ; i < n ; ++i) {
+	        cnt -= (expires[i]);
+	        expires[i+6] += cnt*2;
+	        cnt += (cnt * 2);
+	    }
+	    return cnt;
+	}
 };
